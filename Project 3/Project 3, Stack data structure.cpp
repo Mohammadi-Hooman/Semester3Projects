852b2b038ffa7d72a125f08f5ece2cb059a09d8e
Mohammadi-Hooman <mohammadihooman2@gmail.com>
2025-01-08 07:34:17 +0330

    Add files via upload

---
+++ Project 3/Project 3, Stack data structure.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+const int MaxSize = 100;
+int stack[MaxSize];
+int top = -1;
+
+bool isEmpty() {
+	return (top == -1);
+}
+
+void Push(int item) {
+	if (top == MaxSize - 1) {
+		cout << "stack is full";
+	}
+	else {
+		top++;
+		stack[top] = item;
+	}
+
+int Pop() {
+	if (isEmpty()) {
+		cout << "stack is empty";
+		return -1;
+	}
+	else {
+		return stack[top];
+		top--;
+	}
+}
+
+int Peek() {
+	if (isEmpty()) {
+		cout << "stack is empty";
+	else {
+		return stack[0];
+	}
+	}
+}
+}
